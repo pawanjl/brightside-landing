367335c9ba8366735e250e8fdf903fac91778e11
Pawan Lode <pawanjlode@gmail.com>
2025-10-21 01:09:38 +0530

    build: error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Brightside",
-  description: "The only perp app you need",
+  title: "BrightSide",
+  description: "A product of Juicy Labs Ltd - Awesome features, Your control, 0% fees",
   icons: {
     icon: '/favicon.png',
   },
@@ -28,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         {children}
       </body>

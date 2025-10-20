95e213146cccbda4d68c05ba9dca8355b3450d03
Pawan Lode <pawanjlode@gmail.com>
2025-10-21 00:15:13 +0530

    Draft: metadata change

--- src/components/SocialSelector.tsx
+++ src/components/SocialSelector.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useMemo, useState } from "react";
-import { motion } from "motion/react";
+import { motion } from "framer-motion";
 
 export type Platform = {
   name: string;
